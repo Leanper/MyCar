2889ebc8a1164dfc183dc382ba99826afe85ec90
FeyTsien <974490643@qq.com>
2017-08-22 15:25:18 +0800

    丰富了“好友列表”功能（模拟数据展示）

--- app/src/main/java/com/ygst/cenggeche/ui/activity/friendlist/FriendListActivity.java
+++ app/src/main/java/com/ygst/cenggeche/ui/activity/friendlist/FriendListActivity.java
@@ -1,7 +1,24 @@
 package com.ygst.cenggeche.ui.activity.friendlist;
 
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+
+import com.ygst.cenggeche.R;
+import com.ygst.cenggeche.bean.FriendBean;
 import com.ygst.cenggeche.mvp.MVPBaseActivity;
+import com.ygst.cenggeche.recycle.contacts_recycle.CustomItemDecoration;
+import com.ygst.cenggeche.recycle.contacts_recycle.SideBar;
+import com.ygst.cenggeche.recycle.contacts_recycle.itemAnimator.SlideInOutLeftItemAnimator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -11,8 +28,92 @@ import com.ygst.cenggeche.mvp.MVPBaseActivity;
 
 public class FriendListActivity extends MVPBaseActivity<FriendListContract.View, FriendListPresenter> implements FriendListContract.View {
 
+    private RecyclerView rl_recycle_view;
+    private ContactAdapter mAdapter;
+    private CustomItemDecoration decoration;
+    private SideBar side_bar;
+    List<FriendBean> nameList = new ArrayList<>();
+    private LinearLayoutManager layoutManager;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_friend_list;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+        setToolBar(mToolbar, "好友列表", true);
+//        JMessageClient.registerEventReceiver(this);
+        initViews();
+        initEvents();
+    }
+
+
+    public void initViews() {
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mAdapter = new ContactAdapter(this);
+        rl_recycle_view = (RecyclerView) findViewById(R.id.rl_recycle_view);
+        //侧边导航栏
+        side_bar = (SideBar) findViewById(R.id.side_bar);
+        layoutManager = new LinearLayoutManager(this);
+        rl_recycle_view.setLayoutManager(layoutManager);
+        rl_recycle_view.addItemDecoration(decoration = new CustomItemDecoration(this));
+        rl_recycle_view.setItemAnimator(new SlideInOutLeftItemAnimator(rl_recycle_view));
+        initDatas();
+        rl_recycle_view.setAdapter(mAdapter);
+
+    }
+
+    public void initEvents() {
+        side_bar.setIndexChangeListener(new SideBar.indexChangeListener() {
+            @Override
+            public void indexChanged(String tag) {
+                if (TextUtils.isEmpty(tag) || nameList.size() <= 0) return;
+                for (int i = 0; i < nameList.size(); i++) {
+                    if (tag.equals(nameList.get(i).getIndexTag())) {
+                        layoutManager.scrollToPositionWithOffset(i, 0);
+//                        layoutManager.scrollToPosition(i);
+                        return;
+                    }
+                }
+            }
+        });
+    }
+
+    private void initDatas() {
+        String[] names = {"孙尚香", "安其拉", "白起", "不知火舞", "@小马快跑", "_德玛西亚之力_", "妲己", "狄仁杰", "典韦", "韩信",
+                "老夫子", "刘邦", "刘禅", "鲁班七号", "墨子", "孙膑", "孙尚香", "孙悟空", "项羽", "亚瑟",
+                "周瑜", "庄周", "蔡文姬", "甄姬", "廉颇", "程咬金", "后羿", "扁鹊", "钟无艳", "小乔", "王昭君", "虞姬",
+                "李元芳", "张飞", "刘备", "牛魔", "张良", "兰陵王", "露娜", "貂蝉", "达摩", "曹操", "芈月", "荆轲", "高渐离",
+                "钟馗", "花木兰", "关羽", "李白", "宫本武藏", "吕布", "嬴政", "娜可露露", "武则天", "赵云", "姜子牙",};
+        for (String name : names) {
+            FriendBean bean = new FriendBean();
+            bean.setName(name);
+            nameList.add(bean);
+        }
+        //对数据源进行排序
+        CommonUtil.sortData(nameList);
+        //返回一个包含所有Tag字母在内的字符串并赋值给tagsStr
+        String tagsStr = CommonUtil.getTags(nameList);
+        side_bar.setIndexStr(tagsStr);
+        decoration.setDatas(nameList, tagsStr);
+        mAdapter.addAll(nameList);
+    }
+
+    public void add() {
+        FriendBean bean = new FriendBean();
+        bean.setName("安其拉666");
+        nameList.add(bean);
+        //对数据源进行排序
+        CommonUtil.sortData(nameList);
+        //返回一个包含所有Tag字母在内的字符串并赋值给tagsStr
+        String tagsStr = CommonUtil.getTags(nameList);
+        side_bar.setIndexStr(tagsStr);
+        decoration.setDatas(nameList, tagsStr);
+        //这里写死位置1 只是为了看动画效果
+        mAdapter.add(bean, 1);
     }
 }
