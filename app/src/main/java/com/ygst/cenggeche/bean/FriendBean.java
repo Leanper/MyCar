0df87ab84d537d5382866759eb2b17b9e5ca2e55
FeyTsien <974490643@qq.com>
2017-08-22 15:32:37 +0800

    好友列表的Bean

---
+++ app/src/main/java/com/ygst/cenggeche/bean/FriendBean.java
@@ -0,0 +1,30 @@
+package com.ygst.cenggeche.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/5/12.
+ */
+
+public class FriendBean implements Serializable {
+
+    private String indexTag;
+    private String name;
+
+    public String getIndexTag() {
+        return indexTag;
+    }
+
+    public void setIndexTag(String indexTag) {
+        this.indexTag = indexTag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
