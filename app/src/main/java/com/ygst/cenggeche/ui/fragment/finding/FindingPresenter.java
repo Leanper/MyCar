86d265705de9707960961dc927efb877c7e9d988
FeyTsien <974490643@qq.com>
2017-08-22 15:22:19 +0800

    添加了“发现”的Fragment

---
+++ app/src/main/java/com/ygst/cenggeche/ui/fragment/finding/FindingPresenter.java
@@ -0,0 +1,14 @@
+package com.ygst.cenggeche.ui.fragment.finding;
+
+import android.content.Context;
+
+import com.ygst.cenggeche.mvp.BasePresenterImpl;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class FindingPresenter extends BasePresenterImpl<FindingContract.View> implements FindingContract.Presenter{
+    
+}
