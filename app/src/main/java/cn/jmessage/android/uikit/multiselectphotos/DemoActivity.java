c68921bf7f7164ba47d5d4bb3558032fe0d02421
FeyTsien <974490643@qq.com>
2017-08-23 01:28:12 +0800

    添加了选择发送图片功能，暂时走不通，还需要调整

---
+++ app/src/main/java/cn/jmessage/android/uikit/multiselectphotos/DemoActivity.java
@@ -0,0 +1,54 @@
+package cn.jmessage.android.uikit.multiselectphotos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.ygst.cenggeche.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoActivity extends Activity implements PickPictureActivity.OnSelectedListener {
+
+    public static final int RESULT_CODE_SELECT_PICTURE = 8;
+    public static final int REQUEST_CODE_SELECT_ALBUM = 10;
+    private static final String PICTURE_PATH = "picturePath";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jmui_activity_mult);
+
+        Button startBtn = (Button) findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(DemoActivity.this, AlbumListActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SELECT_ALBUM);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //得到从选择图片返回的Intent
+        if (resultCode == RESULT_CODE_SELECT_PICTURE) {
+            //得到图片路径
+            ArrayList<String> pathList = data.getStringArrayListExtra(PICTURE_PATH);
+            for (String path : pathList) {
+                Log.d("DemoActivity", "path : " + path);
+            }
+        }
+    }
+
+    @Override
+    public void onSelectedPictures(List<String> list) {
+        Log.d("DemoActivity", list.toString());
+    }
+}
